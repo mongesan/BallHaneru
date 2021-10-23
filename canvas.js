dd623f85d006014420a7834a1de752b98508faa3
sakamoto <syun1.mongesan@gmail.com>
2021-10-23 17:06:57 +0900

    平行移動する四角形

--- canvas.js
+++ canvas.js
@@ -1,14 +1,38 @@
-const canvas = document.getElementById("game")
+TEST1 = 0
+
 WIDTH = window.screen.width
 HEIGHT = window.screen.height
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
-const ctx = canvas.getContext('2d');
+X = 0
+Y = HEIGHT/2
 
+window.onload = function () {
+    const canvas = document.getElementById("game")
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+}
 
 function loop(timestamp) {
-    context.clearRect(0, 0, WIDTH, HEIGHT);
-    
+    const canvas = document.getElementById("game")
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    ctx.beginPath();
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    ctx.rect(X, Y, 50, 50);
+
+    if (X > WIDTH-50){
+        TEST1 = 1
+    } else if(X < 0) {
+        TEST1 = 0
+    }
+
+    if (TEST1 == 0){
+        X += 10
+    } else {
+        X -= 10
+    }
+    ctx.fill();
     window.requestAnimationFrame((ts) => loop(ts))
 }
+
 window.requestAnimationFrame((ts) => loop(ts));
